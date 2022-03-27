05b6c69fe7cb99df8cac3fa0b604dc63e4ba86b9
Rishab Arora <29125087+grobo021@users.noreply.github.com>
2022-03-27 22:32:31 +0530

    changes

--- content/content.js
+++ content/content.js
@@ -7,32 +7,41 @@ const renderBubble = (range, text) => {
     controlsDOM.style.top = `calc(${rect.top}px - 50px)`;
     controlsDOM.style.left = `calc(${rect.left}px + calc(${rect.width}px / 2) - 40px)`;
 
-    controlsDOM.innerHTML = text;
+    controlsDOM.innerHTML = `<audio controls><source src=${text} type="text/plain"></source></audio>`;
     controlsDOM.style.visibility = 'visible';
+}
+
+const replaceText = (range, text) => {
+    range.deleteContents();
+    range.insertNode(document.createTextNode(text));
 }
 
 const msg = new SpeechSynthesisUtterance();
 
-document.addEventListener("mouseup", (e) => {
-    let selection = window.getSelection();
-    let text = selection.toString().trim();
+document.addEventListener("mouseup", async (e) => {
+    const selection = window.getSelection();
+    const text = selection.toString().trim();
+    if (text && text != "") {
+        const { options } = await chrome.storage.sync.get("options");
 
-    if (text != "") {
-        let range = selection.getRangeAt(0).cloneRange();
+        const range = selection.getRangeAt(0).cloneRange();
         selection.removeAllRanges();
         selection.addRange(range);
 
-        let lang = "hi";
-        chrome.runtime.sendMessage({ method: "translateSelection", body: text, lang: lang }, (response) => {
-            text = response;
-            range.deleteContents();
-            range.insertNode(document.createTextNode(response));
+        if (options.enableTranslation) {
+            chrome.runtime.sendMessage({ method: "translateSelection", body: text, lang: options.lang }, response => {
+                replaceText(range, response);
 
+                msg.text = response;
+                msg.lang = options.lang;
+                window.speechSynthesis.speak(msg);
+                renderBubble(range, text);
+            });
+        } else {
             msg.text = text;
-            msg.lang = lang;
             window.speechSynthesis.speak(msg);
             renderBubble(range, text);
-        });
+        }
     }
 });
 
