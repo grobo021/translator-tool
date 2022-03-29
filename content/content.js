52ee452daea53c3a0538748374f9661f5502ce72
Rishab Arora <29125087+grobo021@users.noreply.github.com>
2022-03-29 17:53:31 +0530

    bug fix

--- content/content.js
+++ content/content.js
@@ -1,16 +1,3 @@
-const controlsDOM = document.createElement('article');
-controlsDOM.setAttribute('class', 'controls');
-document.body.appendChild(controlsDOM);
-
-const renderBubble = (range, text) => {
-    let rect = range.getBoundingClientRect();
-    controlsDOM.style.top = `calc(${rect.top}px - 50px)`;
-    controlsDOM.style.left = `calc(${rect.left}px + calc(${rect.width}px / 2) - 40px)`;
-
-    controlsDOM.innerHTML = `<audio controls><source src=${text} type="text/plain"></source></audio>`;
-    controlsDOM.style.visibility = 'visible';
-}
-
 const replaceText = (range, text) => {
     range.deleteContents();
     range.insertNode(document.createTextNode(text));
@@ -22,26 +9,26 @@ document.addEventListener("mouseup", async (e) => {
     const selection = window.getSelection();
     const text = selection.toString().trim();
     if (text && text != "") {
-        const { options } = await chrome.storage.sync.get("options");
+        chrome.storage.sync.get(["lang", "enableTranslation"], (options) => {
+            const range = selection.getRangeAt(0).cloneRange();
+            selection.removeAllRanges();
+            selection.addRange(range);
 
-        const range = selection.getRangeAt(0).cloneRange();
-        selection.removeAllRanges();
-        selection.addRange(range);
+            console.log(options);
+            if (options.enableTranslation) {
+                const req = { method: "translateSelection", body: text, lang: options.lang };
+                chrome.runtime.sendMessage(req, response => {
+                    replaceText(range, response);
 
-        if (options.enableTranslation) {
-            chrome.runtime.sendMessage({ method: "translateSelection", body: text, lang: options.lang }, response => {
-                replaceText(range, response);
-
-                msg.text = response;
-                msg.lang = options.lang;
+                    msg.text = response;
+                    msg.lang = options.lang;
+                    window.speechSynthesis.speak(msg);
+                });
+            } else {
+                msg.text = text;
                 window.speechSynthesis.speak(msg);
-                renderBubble(range, text);
-            });
-        } else {
-            msg.text = text;
-            window.speechSynthesis.speak(msg);
-            renderBubble(range, text);
-        }
+            }
+        });
     }
 });
 
